e8f52f01316fb53600dd8bb37dbf4fc23e148ee3
Anthony Oleinik <antholeinik@gmail.com>
2023-02-07 15:15:28 -0600

    trying mjs

--- server/build.js
+++ server/build.js
@@ -3,7 +3,7 @@ const ignorePlugin = require("esbuild-plugin-ignore");
 require("esbuild").build({
     entryPoints: [process.env.LOCAL ? "src/_local.ts" : "src/worker.ts"],
     bundle: true,
-    outfile: process.env.LOCAL ? "dist/_worker.js" : "../client/dist/_worker.js",
+    outfile: process.env.LOCAL ? "dist/_worker.mjs" : "../client/dist/_worker.mjs",
     target: "chrome96",
     plugins: [
         ignorePlugin([
