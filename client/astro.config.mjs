afcbed39f9ef697162d12e7e1c9af89efbc983bc
Anthony Oleinik <antholeinik@gmail.com>
2023-03-10 19:11:52 -0600

    WIP: demo:

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,5 +1,29 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://astro.build/config
 export default defineConfig({
-    site: process.env.SITE,
+  site: process.env.SITE,
+  integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // path to your scss variables
+          additionalData: `@import "@/styles/vars.scss";`
+        }
+      }
+    }
+  }
 });
--- client/src/lib/greet.ts
+++
@@ -1,7 +0,0 @@
-import { client } from "lib/trpc_client";
-
-export const greet = async () => {
-    console.log(await client.greetings.query({
-        name: "Hannah"
-    }))
-}
